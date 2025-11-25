a45be00ac4b3a3fcb492fc5c0ff1c84c67f34eb5
denissragalj-private <denis.sragalj@gmail.com>
2025-11-25 20:00:27 +0100

    qqq

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask
 
-# 1. korak - kreiranje web aplikacije
-
+# 1. korak - kreiranje Flask web aplikacije
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-    return '<h1>Dobro došli u Flask web aplikaciju.</h1>'  # ovo ne želimo ovako raditi
+    return '<h1>Dobro dosli u Flask web demo aplikaciju.</h1>'
+
 
 @app.route('/about-us')
 def about_us():
-    return '<h2>About us u Flask web aplikaciju.</h2>'  # ovo ne želimo ovako raditi
+    return '<h2>About us u Flask web demo aplikaciji.</h2>'
+
+
+@app.route('/hello/<name>')
+def hello (name):
+    return f'Pozdrav, {name}!'
+
+
+@app.route('/sqrt/<int:number>')
+def sqrt (number):
+    return f'Kvadrat {number} je {number ** 2}!'
+
 
 
 if __name__ == '__main__':
