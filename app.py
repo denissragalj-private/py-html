29efc7f6fc934a3dff83bdaf882f0cbe156f7466
denissragalj-private <denis.sragalj@gmail.com>
2025-11-25 20:16:41 +0100

    finalno

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ def index():
     return '<h1>Dobro dosli u Flask web demo aplikaciju.</h1>'
 
 
-@app.route('/about-us')
+@app.route('/about-us')               # prikaz u web pregledniku na adresi http://127.0.0.1:5000/about-us   i daje: About us u Flask web demo aplikaciji.
 def about_us():
     return '<h2>About us u Flask web demo aplikaciji.</h2>'
 
 
-@app.route('/hello/<name>')
+@app.route('/hello/<name>')           # prikaz u web pregledniku na adresi http://127.0.0.1:5000/hello/Denis   i daje: Pozdrav, Denis!
 def hello (name):
     return f'Pozdrav, {name}!'
 
 
-@app.route('/sqrt/<int:number>')
+@app.route('/sqrt/<int:number>')      # prikaz u web pregledniku na adresi  http://127.0.0.1:5000/sqrt/2   i daje ispis: Kvadrat 2 je 4!
 def sqrt (number):
     return f'Kvadrat {number} je {number ** 2}!'
 
